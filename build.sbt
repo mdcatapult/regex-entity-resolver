d7b7803208279b402d367a510aebbed6e6ff5316
Nick Etherington <nick.etherington@md.catapult.org.uk>
2021-03-10 10:39:23 +0000

    Removes Spark stuff.

--- build.sbt
+++ build.sbt
@@ -1,18 +1,13 @@
 name := "mdc-project-resolver"
 version := "0.1"
-scalaVersion := "2.11.12"
+scalaVersion := "2.13.3"
 
 libraryDependencies ++= {
-  val sparkVer = "2.4.7"
-  val sparkNLP = "2.7.1"
   val AkkaVersion = "2.6.4"
   val AkkaHttpVersion = "10.2.0"
 
 
   Seq(
-    "org.apache.spark" %% "spark-core" % sparkVer,
-    "org.apache.spark" %% "spark-mllib" % sparkVer,
-    "com.johnsnowlabs.nlp" %% "spark-nlp" % sparkNLP,
     "org.apache.poi" % "poi" % "5.0.0",
     "org.apache.poi" % "poi-ooxml" % "5.0.0",
     "io.spray" %% "spray-json" % "1.3.5",
--- src/main/scala/io/mdcatapult/resolver/webservice/utils/ProjectCodeResolver.scala
+++ src/main/scala/io/mdcatapult/resolver/webservice/utils/ProjectCodeResolver.scala
@@ -1,54 +1,9 @@
 package io.mdcatapult.resolver.webservice.utils
 
-import com.johnsnowlabs.nlp.annotators.RegexMatcher
-import com.johnsnowlabs.nlp.{DocumentAssembler, SparkNLP}
-import io.mdcatapult.resolver.webservice.model.{Element, Project}
-import org.apache.spark.ml.Pipeline
-import spray.json.enrichAny
-
-import scala.collection.mutable.ArrayBuffer
 
 object ProjectCodeResolver {
 
   def main(args: Array[String]): Unit = {
 
-    val ProjectCodes: String =
-      "MDCP-0199. MDCP-0167."
-
-    val sparkSession = SparkNLP.start()
-    val sqlContext = sparkSession.sqlContext
-    import sqlContext.implicits._
-
-    val dataframe = Seq(ProjectCodes).toDF("text")
-
-    val documentAssembler = new DocumentAssembler()
-      .setInputCol("text")
-      .setOutputCol("document")
-
-    val regexTokenizer = new RegexMatcher()
-      .setInputCols("document")
-      .setOutputCol("regex")
-      .setStrategy("MATCH_ALL")
-      .setRules("src/main/resources/regex.txt", "=")
-
-    val regexPipeline = new Pipeline()
-      .setStages(
-        Array(documentAssembler, regexTokenizer)
-      )
-
-
-    regexPipeline
-      .fit(dataframe)
-      .transform(dataframe)
-      .select("regex")
-      .as[Array[Element]]
-      .foreach(elementArray => {
-        val projects = ArrayBuffer[Project]()
-        elementArray.foreach(element => {
-          projects += Project(element.result, element.metaData("identifier"))
-        })
-        print(projects.toArray.toJson)
-        projects.toArray.toJson
-      })
   }
 }
