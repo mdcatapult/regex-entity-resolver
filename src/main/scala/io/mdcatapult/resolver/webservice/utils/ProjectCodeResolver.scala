47ec1ad3e85905d9e3c8e42497f59a28683d822c
Nick Etherington <nick.etherington@md.catapult.org.uk>
2021-03-11 14:59:37 +0000

    Adds tests.

--- src/main/scala/io/mdcatapult/resolver/webservice/utils/ProjectCodeResolver.scala
+++ src/main/scala/io/mdcatapult/resolver/webservice/utils/ProjectCodeResolver.scala
@@ -1,15 +1,18 @@
 package io.mdcatapult.resolver.webservice.utils
 
 import io.mdcatapult.resolver.webservice.model.Project
-
 import scala.io.Source
-
 
 object ProjectCodeResolver {
 
-  val projectCodeMap: Map[String, String] = createProjectCodeMap("src/test/resources/codes.txt")
+  val projectCodeMap: Map[String, String] = createProjectCodeMapFromTextFile("src/test/resources/codes.txt")
 
-  private def createProjectCodeMap(filepath: String): Map[String, String] = {
+  /**
+   *
+   * @param filepath Path to text file in the format projectCode|=|projectName
+   * @return Map with projectCode|projectName key value pairs
+   */
+  private def createProjectCodeMapFromTextFile(filepath: String): Map[String, String] = {
 
     val source = Source.fromFile(filepath)
     val lines = source.getLines.toList
@@ -25,17 +28,25 @@ object ProjectCodeResolver {
 
   val mdcProjectRegex = "\\bMDCP-\\d{4}\\b".r
 
-  def resolve(text: String): Unit = {
+  /**
+   * Given a string, identifies MDC project codes and resolves them to project name
+   *
+   * @param text String in which codes are to be identified
+   * @return List of maps with keys of 'code' and 'name'
+   */
+  def resolve(text: String): List[Project] = {
 
+    // find codes in the correct format
     val matches: List[String] = mdcProjectRegex.findAllIn(text).toList
+    // lookup names from codes
     val codes: List[(String, Option[String])] = matches.map(singleMatch => {
       singleMatch -> projectCodeMap.get(singleMatch)
     })
 
+    // ensure we only return codes which resolve to a project
     codes.collect {
       case (code, Some(name)) => Project(code, name)
     }
   }
-
 
 }
--- src/test/scala/io/mdcatapult/resolver/webservice/ProjectResolverSpec.scala
+++ src/test/scala/io/mdcatapult/resolver/webservice/ProjectResolverSpec.scala
@@ -8,12 +8,27 @@ import org.scalatest.wordspec.AnyWordSpec
 class ProjectResolverSpec extends AnyWordSpec with Matchers {
 
   val exampleCode = "Some text. MDCP-0167. Some other stuff."
+  val exampleNonCode = "Some text. MDCP-01678. Some other stuff."
+  val exampleInexistentCode = "Some text. MDCP-9999. Some other stuff."
+  val exampleMultipleCodes = "Some text. MDCP-0167. Some other stuff. MDCP-0159."
 
 
   "The projectResolver" should {
-    "" in {
-      ProjectCodeResolver.resolve(exampleCode)
-//      assert(true)
+    "resolve a single project code" in {
+      assert(ProjectCodeResolver.resolve(exampleCode).length === 1)
     }
+
+    "resolve multiple project codes" in {
+      assert(ProjectCodeResolver.resolve(exampleMultipleCodes).length === 2)
+    }
+
+    "not resolve a project code in the correct format but for which there is no MDC project" in {
+      assert(ProjectCodeResolver.resolve(exampleInexistentCode).length === 0)
+    }
+
+    "not resolve a code which is not in the correct format" in {
+      assert(ProjectCodeResolver.resolve(exampleNonCode).length === 0)
+    }
+
   }
 }
