d40a3ccf8eb7561024855667fcd3a3b9556c40fa
Ian Dunlop <ian.dunlop@md.catapult.org.uk>
2021-07-07 16:01:58 +0100

    There could be multiple matches so regex resolve the correct one

--- src/main/scala/io/mdcatapult/resolver/webservice/utils/ProjectCodeResolver.scala
+++ src/main/scala/io/mdcatapult/resolver/webservice/utils/ProjectCodeResolver.scala
@@ -17,9 +17,10 @@ class ProjectCodeResolver(mdcProjectRegex: Regex, projectCodeMap: Map[String, St
     val matches: List[String] = mdcProjectRegex.findAllIn(text).toList
     // lookup names from codes
     val codes: List[(String, Option[String])] = matches.map(singleMatch => {
+      // Are we using the regex to resolve the entity
       if (config.resolverMatchFromRegex) {
-        val actualMatch = config.resolverMatchRegex.r.findAllIn(text).toList.head
-        val textWithoutMatch = text.replace(actualMatch, "")
+        val actualMatch = config.resolverMatchRegex.r.findAllIn(singleMatch).toList.head
+        val textWithoutMatch = singleMatch.replace(actualMatch, "")
         singleMatch -> projectCodeMap.get(textWithoutMatch)
       } else {
         singleMatch -> projectCodeMap.get(singleMatch)
